5db3e9c0201051ed10a9a96274bd5d5ca894380a
Piotr Latuszkiewicz <piotr.latuszkiewicz@gmail.com>
2021-02-23 18:36:44 +0100

    cofnij rem removeAds

--- js/main.js
+++ js/main.js
@@ -75,12 +75,12 @@ function isVerticalView() {
 }
 
 function removeAds() {
-   // if ($("body").children().length != 1) { // if localhost
-   //     while ($("body").children().length != 2) {
-   //         $("body").children()[$("body").children().length - 1].remove();
-   //     }
-   //     $.each($('center'), function (name, val) {
-   //         $(val).remove();
-   //     });
-   // }
+    if ($("body").children().length != 1) { // if localhost
+        while ($("body").children().length != 2) {
+            $("body").children()[$("body").children().length - 1].remove();
+        }
+        $.each($('center'), function (name, val) {
+            $(val).remove();
+        });
+    }
 }
